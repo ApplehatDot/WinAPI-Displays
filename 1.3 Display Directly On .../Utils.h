81b8a11424a1837219a74a9cc35ecad75861f585
ApplehatDoesStuff <152534245+ApplehatDot@users.noreply.github.com>
2025-01-05 17:00:32 +0100

    Create Utils.h

---
+++ 1.3 Display Directly On .../Utils.h
@@ -0,0 +1,18 @@
+/* WinAPI - 1.3 Display Directly On ...
+ *		Github: https://github.com/ApplehatDot
+ * 		Enum and Window Creation functions - header file
+ */
+ 
+#ifndef UTILS_H
+#define UTILS_H
+
+typedef struct {
+    HWND hwnd;
+    TCHAR selectedMonitor[32];
+} MonitorEnumData;
+
+BOOL CALLBACK EnumDisplayMonitorsProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData);
+LRESULT CALLBACK NewWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData);
+
+#endif // UTILS_H
